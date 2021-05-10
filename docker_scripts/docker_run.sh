f6b838b9571a440f6e04e499157d3707a27cd42d
Ben Agro <ben.agro@mail.utoronto.ca>
2021-05-10 11:57:21 -0400

    added collision avoidance in IK

--- docker_scripts/docker_run.sh
+++ docker_scripts/docker_run.sh
@@ -4,9 +4,24 @@ echo ""
 echo "Running drake_workspace container"
 echo ""
 
+port=2400
+name='drake_workspace'
+
+while getopts p:n: option
+do
+    case "${option}"
+        in
+        p) port=${OPTARG};; 
+        n) name=${OPTARG};;
+    esac
+done
+
+echo "Using port: $port"
+echo "Using container name: $name"
+
 sudo docker run -it \
     -v /home/agrobenj/drake_workspace:/home/$USER/workspace \
-    -p 2400:22 \
-    --name drake_workspace \
+    -p $port:22 \
+    --name $name \
     drake_workspace \
     /bin/bash
