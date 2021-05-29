263d3c3db9feff0a1b6ced3f7db9865698b9e13b
Ben Agro <ben.agro@mail.utoronto.ca>
2021-05-29 19:00:18 -0400

    grasping jazz

--- PandaGrasping/PandaGrasping.py
+++ PandaGrasping/PandaGrasping.py
@@ -8,41 +8,69 @@ from pydrake.all import (
 from PandaInverseKinematics import *
 import open3d as o3d
 
-#TODO(ben): add inverse kinematics to cost
 
-def grasp_candidate_cost(X_G, plant_context, cloud, plant, scene_graph, scene_graph_context):
-    
+def grasp_candidate_cost(plant_context, cloud, plant, scene_graph, scene_graph_context):
+
+    def is_minimally_valid(X_G):
+        plant.SetFreeBodyPose(plant_context, body, X_G)
+        query_object = scene_graph.get_query_output_port().Eval(scene_graph_context)
+        
+        if query_object.HasCollisions():
+            return False
+
+        # Check collisions between the gripper and the point cloud
+        margin = 0.0  # must be smaller than the margin used in the point cloud preprocessing.
+        for pt in cloud.points:
+            distances = query_object.ComputeSignedDistanceToPoint(pt, threshold=margin)
+            if distances:
+                return False
+
+        return True
+
+
     body = plant.GetBodyByName("panda_hand")
+    X_G = plant.GetFreeBodyPose(plant_context, body)
 
     X_GW = X_G.inverse()
     pts = np.asarray(cloud.points).T
     p_GC = X_GW.multiply(pts)
 
     # Crop to a region inside of the finger box.
-    crop_min = [-0.009, -0.035, 0.06]
-    crop_max = [0.009, 0.035, 0.115]
+    crop_min = [-0.009, -0.04, 0.066]
+    crop_max = [0.009, 0.04, 0.12]
     indices = np.all((crop_min[0] <= p_GC[0,:], p_GC[0,:] <= crop_max[0],
                       crop_min[1] <= p_GC[1,:], p_GC[1,:] <= crop_max[1],
                       crop_min[2] <= p_GC[2,:], p_GC[2,:] <= crop_max[2]), 
                      axis=0)
 
+    X_G_lower = RigidTransform(X_G)
+    X_G_upper = RigidTransform(X_G)
+    if np.sum(indices)>0:
+        p_GC_y = p_GC[1, indices]
+        p_Gcenter_y = (p_GC_y.min() + p_GC_y.max())/2.0
+        X_G.set_translation(X_G.translation() + X_G.rotation().multiply([0, p_Gcenter_y, 0]))
+        X_G_upper.set_translation(X_G_upper.translation() + X_G_upper.rotation().multiply([0, p_GC_y.min() + 0.04, 0]))
+        X_GW = X_G.inverse()
 
+    if not is_minimally_valid(X_G):
+        return np.inf, None, None, None
 
-    query_object = scene_graph.get_query_output_port().Eval(scene_graph_context)
-    collision_pairs = query_object.ComputePointPairPenetration()
-    
-    # check collisions with objects in the world 
-    if query_object.HasCollisions():
-        print('collision with world')
-        return np.inf
+    # we know the grasp is valid, lets find a range of valid z values 
+    # (for flexibility in the IK solution)
 
-    # Check collisions between the gripper and the point cloud
-    margin = 0.0  # must be smaller than the margin used in the point cloud preprocessing.
-    for pt in cloud.points:
-        distances = query_object.ComputeSignedDistanceToPoint(pt, threshold=margin)
-        if distances:
-            print('collision with point cloud')
-            return np.inf
+    p_GC_z = p_GC[2, indices]
+    zs = np.linspace(p_GC_z.min() - 0.0995, p_GC_z.min() - 0.066, 10)
+
+    valids = []
+    for p_Gzshift in zs:
+        X_Gtest = RigidTransform(X_G)
+        X_Gtest.set_translation(X_Gtest.translation() + X_Gtest.rotation().multiply([0,0,p_Gzshift]))
+        if is_minimally_valid(X_Gtest):
+            valids.append(p_Gzshift)
+
+    if len(valids) != 0:
+        X_G_lower.set_translation(X_G_lower.translation() + X_G_lower.rotation().multiply([0, 0, min(valids)]))
+        X_G_upper.set_translation(X_G_upper.translation() + X_G_upper.rotation().multiply([0, 0, max(valids)]))
 
     n_GC = X_GW.rotation().multiply(np.asarray(cloud.normals)[indices,:].T)
 
@@ -50,58 +78,22 @@ def grasp_candidate_cost(X_G, plant_context, cloud, plant, scene_graph, scene_gr
     # weight * -dot([0, 0, -1], R_G * [0, 1, 0]) = weight * R_G[2,1]
     cost = 20.0*X_G.rotation().matrix()[2, 1]
 
-    # Reward sum |dot product of normals with gripper x|^2
-    cost -= np.sum(n_GC[0,:]**2)
+    # Reward sum |dot product of normals with gripper y|^2
+    cost -= np.sum(n_GC[1,:]**2)
 
-    return cost
-
-def process_bin_point_cloud(diagram, context, cameras, bin_name):
-
-    plant = diagram.GetSubsystemByName("plant")
-    plant_context = plant.GetMyContextFromRoot(context)
-
-    # Compute crop box.
-    bin_instance = plant.GetModelInstanceByName(bin_name)
-    bin_body = plant.GetBodyByName("bin_base", bin_instance)
-    X_B = plant.EvalBodyPoseInWorld(plant_context, bin_body)
-    margin = 0.001  # only because simulation is perfect!
-    a = X_B.multiply([-.22+0.025+margin, -.29+0.025+margin, 0.015+margin])
-    b = X_B.multiply([.22-0.1-margin, .29-0.025-margin, 2.0])
-    crop_min = np.minimum(a,b)
-    crop_max = np.maximum(a,b)
-
-    # Evaluate the camera output ports to get the images.
-    merged_pcd = o3d.geometry.PointCloud()
-    for c in cameras:
-        point_cloud = diagram.GetOutputPort(f"{c}_point_cloud").Eval(context)
-        pcd = create_open3d_point_cloud(point_cloud)
-
-        # Crop to region of interest.
-        pcd = pcd.crop(
-            o3d.geometry.AxisAlignedBoundingBox(min_bound=crop_min,
-                                                max_bound=crop_max))    
-
-        pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(
-            radius=0.1, max_nn=30))
-
-        camera = plant.GetModelInstanceByName(c)
-        body = plant.GetBodyByName("base", camera)
-        X_C = plant.EvalBodyPoseInWorld(plant_context, body)
-        pcd.orient_normals_towards_camera_location(X_C.translation())
-        
-        # Merge point clouds.
-        merged_pcd += pcd
-
-    # Voxelize down-sample.  (Note that the normals still look reasonable)
-    return merged_pcd.voxel_down_sample(voxel_size=0.005)
-
-
-
-def generate_grasp_candidate_antipodal(plant_context, cloud, plant, scene_graph, scene_graph_context, panda, rng, meshcat=None):
+    return cost, X_G_lower, X_G, X_G_upper
+    
+def generate_grasp_candidate_antipodal(env, env_context, cloud, rng): 
     """
     Picks a random point in the cloud, and aligns the robot finger with the normal of that pixel. 
     The rotation around the normal axis is drawn from a uniform distribution over [min_roll, max_roll].
     """
+    
+    plant = env.get_multibody_plant()
+    plant_context = env.GetSubsystemContext(plant, env_context)
+    scene_graph = env.get_scene_graph()
+    scene_graph_context = env.GetSubsystemContext(scene_graph, env_context)
+
     body = plant.GetBodyByName("panda_hand")
 
     index = rng.integers(0,len(cloud.points)-1)
@@ -110,66 +102,42 @@ def generate_grasp_candidate_antipodal(plant_context, cloud, plant, scene_graph,
     p_WS = np.asarray(cloud.points[index])
     n_WS = np.asarray(cloud.normals[index])
 
-    if meshcat:
-        vertices = np.empty((3,2))
-        vertices[:, 0] = p_WS
-        vertices[:, 1] = p_WS + 0.05*n_WS
-        meshcat.set_object(g.LineSegments(g.PointsGeometry(vertices),
-                            g.MeshBasicMaterial(color=0xff0000)))
-  
-
-    if not np.isclose(np.linalg.norm(n_WS), 1.0):
-        return np.inf, None
+    assert np.isclose(np.linalg.norm(n_WS), 1.0), "This error is likely because you don't have version 0.10.0.0 of open3d"
 
     Gy = n_WS # gripper y axis aligns with normal
     # make orthonormal z axis, aligned with world down
     z = np.array([0.0, 0.0, -1.0])
     if np.abs(np.dot(z,Gy)) < 1e-6:
-        # normal was pointing straight down.  reject this sample.
-        print('here')
-        return np.inf, None
+        return np.inf, None, None, None
 
     Gz = z - np.dot(z,Gy)*Gy
     Gx = np.cross(Gy, Gz)
     R_WG = RotationMatrix(np.vstack((Gx, Gy, Gz)).T)
-    p_GS_G = [0, 0.035, 0.11]
+    p_GS_G = [0, 0.04, 0.1]
 
     # Try orientations from the center out
     min_pitch=-np.pi/3.0
     max_pitch=np.pi/3.0
     alpha = np.array([0.5, 0.65, 0.35, 0.8, 0.2, 1.0, 0.0])
-    thetas = [0]
-    for theta in (min_pitch + (max_pitch - min_pitch)*alpha):
-        # Rotate the object in the hand by a random rotation (around the normal).
-        R_WG2 = R_WG.multiply(RotationMatrix.MakeYRotation(theta))
+    thetas = (min_pitch + (max_pitch - min_pitch)*alpha)
 
+    for theta in thetas: 
+        # Rotate the object in the hand by a rotation (around the normal).
+        R_WG2 = R_WG.multiply(RotationMatrix.MakeYRotation(theta))
         # Use G for gripper frame.
         p_SG_W = - R_WG2.multiply(p_GS_G)
         p_WG = p_WS + p_SG_W 
 
         X_G = RigidTransform(R_WG2, p_WG)
-        ik = PandaInverseKinematics(plant, plant_context, panda)
-        p_WQ = X_G.translation()
-        tol = np.ones(3)*0.01
-        q_nominal = np.array([ 0., 0.55, 0., -1.45, 0., 1.58, 0.]) 
-        ik.AddPositionConstraint(p_WQ+tol, p_WQ+tol)
-        ik.AddOrientationConstraint(X_G.rotation(), 0.01)
-        prog = ik.get_prog()
-        q = ik.get_q()
-        prog.AddQuadraticErrorCost(np.identity(len(q)), q_nominal, q)
-        prog.SetInitialGuess(q, q_nominal)
-        result = Solve(prog)
-
-        if not result.is_success():
-            continue
         
-        plant.SetPositions(plant_context, panda, result.GetSolution(q))
-        print('evaluating cost')
-        cost = grasp_candidate_cost(X_G, plant_context, cloud, plant, scene_graph, scene_graph_context)
-        #X_G = plant.GetFreeBodyPose(plant_context, body)
+        plant.SetFreeBodyPose(plant_context, body, X_G)
+
+        cost, X_G_lower, X_G, X_G_upper = grasp_candidate_cost(plant_context, cloud, plant, scene_graph, scene_graph_context)
+        
         if np.isfinite(cost):
-            return cost, X_G
+            return cost, X_G_lower, X_G, X_G_upper
 
-        #draw_grasp_candidate(X_G)
+    return np.inf, None, None, None
 
-    return np.inf, None
+
+
--- PandaStation/PandaStation.py
+++ PandaStation/PandaStation.py
@@ -1,5 +1,9 @@
 import pydrake.all
-from pydrake.all import LoadModelDirectives, ProcessModelDirectives, GeometrySet
+from pydrake.all import (
+        LoadModelDirectives, ProcessModelDirectives, GeometrySet, Parser,
+        DiagramBuilder, AddMultibodyPlantSceneGraph, Diagram, MultibodyPlant,
+        Demultiplexer, InverseDynamicsController, Adder, PassThrough,    
+        StateInterpolatorWithDiscreteDerivative)
 from .scenarios import AddPanda, AddPandaHand, AddRgbdSensors
 from .panda_hand_position_controller import PandaHandPositionController, MakeMultibodyStateToPandaHandStateSystem
 from .utils import FindResource, AddPackagePaths
@@ -9,25 +13,28 @@ import os
 def deg_to_rad(deg):
     return deg*np.pi/180.0
 
-class PandaStation(pydrake.systems.framework.Diagram):
+class PandaStation(Diagram):
 
     def __init__(self, time_step = 0.002):
 
-        pydrake.systems.framework.Diagram.__init__(self)
+        Diagram.__init__(self)
 
         self.time_step = time_step
 
-        self.builder = pydrake.systems.framework.DiagramBuilder()
-        self.plant, self.scene_graph = pydrake.multibody.plant.AddMultibodyPlantSceneGraph(
+        self.builder = DiagramBuilder()
+        self.plant, self.scene_graph = AddMultibodyPlantSceneGraph(
                                                         self.builder, time_step = self.time_step)
-        self.setup = None
         self.plant.set_name("plant")
         self.set_name("panda_station")
         self.object_ids = []
         self.object_poses = []
         self.camera_info = {} #dictionary in the form name: pose
 
-        self.controller_plant = pydrake.multibody.plant.MultibodyPlant(time_step = self.time_step)
+        self.controller_plant = MultibodyPlant(time_step = self.time_step)
+        self.welded_hand = False
+
+        # this environment has only the hand, for grasp planning
+        self.hand_env = HandEnv()
 
 
     def Finalize(self):
@@ -35,6 +42,8 @@ class PandaStation(pydrake.systems.framework.Diagram):
 
         assert self.panda.is_valid(), "No panda model added"
         assert self.hand.is_valid(), "No panda hand model added"
+
+        self.hand_env.Finalize()
 
 
         self.plant.Finalize()
@@ -47,13 +56,13 @@ class PandaStation(pydrake.systems.framework.Diagram):
         num_panda_positions = self.plant.num_positions(self.panda)
 
         # add a "pass through" to the system. A pass through is input -> output
-        panda_position = self.builder.AddSystem(pydrake.systems.primitives.PassThrough(num_panda_positions))
+        panda_position = self.builder.AddSystem(PassThrough(num_panda_positions))
         self.builder.ExportInput(panda_position.get_input_port(), "panda_position")
         self.builder.ExportOutput(panda_position.get_output_port(), "panda_position_command")
 
         # export panda state outputs 
         # demux with inputs for panda joint positions and velocities (panda state)
-        demux = self.builder.AddSystem(pydrake.systems.primitives.Demultiplexer(
+        demux = self.builder.AddSystem(Demultiplexer(
             2 * num_panda_positions, num_panda_positions))
         self.builder.Connect(self.plant.get_state_output_port(self.panda), demux.get_input_port())
         # sticking to naming conventions from manipulation station for ports
@@ -67,8 +76,7 @@ class PandaStation(pydrake.systems.framework.Diagram):
         self.controller_plant.Finalize()
 
         # add panda controller. TODO(ben): make sure that this controller is realistic
-        panda_controller = self.builder.AddSystem(
-            pydrake.systems.controllers.InverseDynamicsController(
+        panda_controller = self.builder.AddSystem(InverseDynamicsController(
                 self.controller_plant,
                 kp =[100]*num_panda_positions,
                 ki =[1]*num_panda_positions,
@@ -79,38 +87,39 @@ class PandaStation(pydrake.systems.framework.Diagram):
         self.builder.Connect(self.plant.get_state_output_port(self.panda), panda_controller.get_input_port_estimated_state())
 
         # feedforward torque
-        adder = self.builder.AddSystem(pydrake.systems.primitives.Adder(2, num_panda_positions))
+        adder = self.builder.AddSystem(Adder(2, num_panda_positions))
         self.builder.Connect(panda_controller.get_output_port_control(),
                         adder.get_input_port(0))
         # passthrough to make the feedforward torque optional (default to zero values)
         torque_passthrough = self.builder.AddSystem(
-            pydrake.systems.primitives.PassThrough([0]*num_panda_positions))
+            PassThrough([0]*num_panda_positions))
         self.builder.Connect(torque_passthrough.get_output_port(), adder.get_input_port(1))
         self.builder.ExportInput(torque_passthrough.get_input_port(), "panda_feedforward_torque")
         self.builder.Connect(adder.get_output_port(), self.plant.get_actuation_input_port(self.panda))
 
         # add a discete derivative to find velocity command based on positional commands
         desired_state_from_position = self.builder.AddSystem(
-            pydrake.systems.primitives.StateInterpolatorWithDiscreteDerivative(
+            StateInterpolatorWithDiscreteDerivative(
                 num_panda_positions, self.time_step, suppress_initial_transient = True))
         desired_state_from_position.set_name("desired_state_from_position")
         self.builder.Connect(desired_state_from_position.get_output_port(),
                         panda_controller.get_input_port_desired_state())
         self.builder.Connect(panda_position.get_output_port(), desired_state_from_position.get_input_port())
 
-        #TODO(ben): make sure this hand controller is accurate
-        hand_controller = self.builder.AddSystem(PandaHandPositionController())
-        hand_controller.set_name("hand_controller")
-        self.builder.Connect(hand_controller.GetOutputPort("generalized_force"),             
-                        self.plant.get_actuation_input_port(self.hand))
-        self.builder.Connect(self.plant.get_state_output_port(self.hand), hand_controller.GetInputPort("state"))
-        self.builder.ExportInput(hand_controller.GetInputPort("desired_position"), "hand_position")
-        self.builder.ExportInput(hand_controller.GetInputPort("force_limit"), "hand_force_limit")
-        hand_mbp_state_to_hand_state = self.builder.AddSystem(
-                                                MakeMultibodyStateToPandaHandStateSystem())
-        self.builder.Connect(self.plant.get_state_output_port(self.hand), hand_mbp_state_to_hand_state.get_input_port())
-        self.builder.ExportOutput(hand_mbp_state_to_hand_state.get_output_port(), "hand_state_measured")
-        self.builder.ExportOutput(hand_controller.GetOutputPort("grip_force"), "hand_force_measured")
+        if not self.welded_hand: # no need to do this if the hands are welded
+            #TODO(ben): make sure this hand controller is accurate
+            hand_controller = self.builder.AddSystem(PandaHandPositionController())
+            hand_controller.set_name("hand_controller")
+            self.builder.Connect(hand_controller.GetOutputPort("generalized_force"),             
+                            self.plant.get_actuation_input_port(self.hand))
+            self.builder.Connect(self.plant.get_state_output_port(self.hand), hand_controller.GetInputPort("state"))
+            self.builder.ExportInput(hand_controller.GetInputPort("desired_position"), "hand_position")
+            self.builder.ExportInput(hand_controller.GetInputPort("force_limit"), "hand_force_limit")
+            hand_mbp_state_to_hand_state = self.builder.AddSystem(
+                                                    MakeMultibodyStateToPandaHandStateSystem())
+            self.builder.Connect(self.plant.get_state_output_port(self.hand), hand_mbp_state_to_hand_state.get_input_port())
+            self.builder.ExportOutput(hand_mbp_state_to_hand_state.get_output_port(), "hand_state_measured")
+            self.builder.ExportOutput(hand_controller.GetOutputPort("grip_force"), "hand_force_measured")
 
         # add any cameras
 
@@ -141,13 +150,13 @@ class PandaStation(pydrake.systems.framework.Diagram):
         plant_context = self.GetSubsystemContext(self.plant, station_context)
         return self.plant.GetPositions(plant_context, self.panda)
 
-    def SetPandaPosition(self, station_context, state, q):
+    def SetPandaPosition(self, station_context, q):
         num_panda_positions = self.plant.num_positions(self.panda)
         assert len(q) == num_panda_positions, "Incorrect size of q, needs to be 7"
 
         plant_context = self.GetSubsystemContext(self.plant, station_context)
-        plant_state = self.GetMutableSubsystemState(self.plant, state)
-        self.plant.SetPositions(plant_context, plant_state, self.panda, q)
+        #plant_state = self.GetMutableSubsystemState(self.plant, state)
+        self.plant.SetPositions(plant_context, self.panda, q)
 
     def SetPandaVelocity(self, station_context, state, v):
         num_panda_positions = self.plant.num_positions(self.panda)
@@ -184,66 +193,122 @@ class PandaStation(pydrake.systems.framework.Diagram):
         self.scene_graph.ExcludeCollisionsBetween(panda_link5, panda_link7)
         self.scene_graph.ExcludeCollisionsBetween(panda_link7, panda_hand)
 
-    def SetupDefaultStation(self):
+    def SetupDefaultStation(self, welded_hand = False):
         self.panda = AddPanda(self.plant)
-        self.hand = AddPandaHand(self.plant, panda_model_instance = self.panda)
-
+        self.hand = AddPandaHand(self.plant, panda_model_instance = self.panda, 
+                welded = welded_hand)
+        self.welded_hand = welded_hand
         self.fix_collisions()
-        
+        self.hand_env.AddHand()
 
-    def SetupBinStation(self):
-        self.setup = "BinStation"
-
+    def SetupBinStation(self, welded_hand = False):
         directive = FindResource("models/two_bins_w_cameras.yaml")
-        parser = pydrake.multibody.parsing.Parser(self.plant)
+        parser = Parser(self.plant)
         AddPackagePaths(parser)
 
         # adds bins and cameras
         ProcessModelDirectives(LoadModelDirectives(directive), self.plant, parser)
 
+        self.hand_env.SetupFromFile("models/two_bins_w_cameras.yaml")
         # adds hand and arm
-        self.SetupDefaultStation() 
+        self.SetupDefaultStation(welded_hand) 
     
-    def SetupTableStation(self):
-        self.setup = "TableStation"
-
+    def SetupTableStation(self, welded_hand = False):
         directive = FindResource("models/table_top.yaml")
-        parser = pydrake.multibody.parsing.Parser(self.plant)
+        parser = Parser(self.plant)
         AddPackagePaths(parser)
 
         # adds bins and cameras
         ProcessModelDirectives(LoadModelDirectives(directive), self.plant, parser)
 
+        self.hand_env.SetupFromFile("models/table_top.yaml")
         # adds hand and arm
-        self.SetupDefaultStation() 
+        self.SetupDefaultStation(welded_hand) 
 
     def SetupStationFromFile(setup_name, file_name):
         """ setup a the station from a yaml file in the models directory """
 
         self.setup = setup_name
         
+        # TODO(ben): generalize this 
         directive = FindResource("models/" + file_name + ".yaml")
-        parser = pydrake.multibody.parsing.Parser(self.plant)
+        parser = Parser(self.plant)
         AddPackagePaths(parser)
 
         # adds bins and cameras
         ProcessModelDirectives(LoadModelDirectives(directive), self.plant, parser)
 
+        self.hand_env.SetupFromFile("models/" + file_name + ".yaml")
         # adds hand and arm
         self.SetupDefaultStation() 
 
-
-    def AddManipulandFromFile(self, model_file, X_WObject):
-        parser = pydrake.multibody.parsing.Parser(self.plant)
-        number = str(len(self.object_ids))
-        name = "added_model_"+number
-        model = parser.AddModelFromFile(pydrake.common.FindResourceOrThrow(model_file), name)
+    def AddModelFromFile(self, path, X_WO, name = None, to_hand_env = False):
+        parser = Parser(self.plant)
+        if name is None:
+            num = str(len(self.object_ids))
+            name = "added_model_" + num
+        model = parser.AddModelFromFile(path, name)
         indices = self.plant.GetBodyIndices(model)
-        # TODO(ben): generalize this to add any model
-        assert len(indices) == 1, "Curently, we only support manipulands with one body"
+        assert len(indices) == 1, "Currently, we only support adding models with one body"
         self.object_ids.append(indices[0])
-        self.object_poses.append(X_WObject)
+        self.object_poses.append(X_WO)
+        if to_hand_env:
+            self.hand_env.AddModelFromFile(path, X_WO, name)
+
+    def get_hand_env(self):
+        return self.hand_env
 
     def get_camera_info(self):
         return self.camera_info
 
+class HandEnv(Diagram):
+    
+    def __init__(self, time_step = 0.001):
+        Diagram.__init__(self)
+        self.time_step = time_step
+        self.builder = DiagramBuilder()
+        self.plant, self.scene_graph = AddMultibodyPlantSceneGraph(self.builder, time_step = self.time_step)
+        self.set_name("hand_env")
+        self.plant.set_name("plant")
+        self.hand = None   
+        self.object_ids = []
+        self.object_poses = []
+        
+    def Finalize(self):
+        assert self.hand.is_valid(), "You forgot to setup the environment"
+        self.plant.Finalize()
+        
+        for i in range(len(self.object_ids)):
+            body_index = self.object_ids[i]
+            body = self.plant.get_body(body_index)
+            self.plant.SetDefaultFreeBodyPose(body, self.object_poses[i])
+        
+        AddRgbdSensors(self.builder, self.plant, self.scene_graph)
+        self.builder.ExportOutput(self.scene_graph.get_query_output_port(), "query_object")
+        self.builder.BuildInto(self)
+
+    def SetupFromFile(self, filename):
+        parser = Parser(self.plant)
+        AddPackagePaths(parser)
+        directive = FindResource(filename)
+        ProcessModelDirectives(LoadModelDirectives(directive), self.plant, parser)
+        
+    def AddModelFromFile(self, path, X_WO, name = None):
+        parser = Parser(self.plant)
+        if name is None:
+            num = str(len(self.object_ids))
+            name = "added_model_" + num
+        model = parser.AddModelFromFile(path, name)
+        indices = self.plant.GetBodyIndices(model)
+        assert len(indices) == 1, "Currently, we only support adding models with one body"
+        self.object_ids.append(indices[0])
+        self.object_poses.append(X_WO)
+        
+    def AddHand(self):
+        self.hand = AddPandaHand(self.plant, welded = True)
+        
+    def get_multibody_plant(self):
+        return self.plant
+    
+    def get_scene_graph(self):
+        return self.scene_graph
