a4f9f706a43499ab7b631c518c3398e472537524
Ben Agro <ben.agro@mail.utoronto.ca>
2021-05-30 20:42:06 -0400

    working on cylinder

--- PandaGrasping/GroundTruthGrasping.py
+++ PandaGrasping/GroundTruthGrasping.py
@@ -137,7 +137,7 @@ def parse_manipuland_shapes(station, station_context):
                 else:
                     shape_infos.append(ShapeInfo(body, X_WB, X_BG, shape))
             if type(shape) == Cylinder:
-                if shape.radius() > 0.04: 
+                if shape.radius() > 0.04 and shape.length() > 0.08: 
                     # we won't consider picking up a cylinder with these dimensions
                     continue 
                 else:
@@ -175,18 +175,141 @@ def grasp_pose(body_info, station, station_context):
         shape_info: a shape info instance
         station: a PandaStation system
     """
-    grasp_qs = []
+    qs = []
+    costs = []
     for shape_info in body_info.shape_infos:
         if not is_graspable(shape_info):
             continue 
         if shape_info.type == Cylinder:
-            grasp_qs.append(cylinder_grasp_pose(shape_info, station, station_context))
+            q, cost = cylinder_grasp_pose(shape_info, station, station_context)
         if shape_info.type == Box:
-            grasp_qs.append(box_grasp_pose(shape_info, station, station_context))
+            q, cost = box_grasp_pose(shape_info, station, station_context)
         if shape_info.type == Sphere:
-            grasp_qs.append(sphere_grasp_pose(shape_info, station, station_context))
+            q, cost = sphere_grasp_pose(shape_info, station, station_context)
+        qs.append(q)
+        print(q)
+        costs.append(cost)
+    indices = np.argsort(costs)
+    return qs[indices[0]], costs[indices[0]]
 
-    return grasp_qs
+def cylinder_grasp_pose(shape_info, station, station_context):
+    """
+    Returns the best generalized coordinates, q (np.array), for the panda
+    in the PandaStation station to grasp the cylinder in shape_info
+
+    Args:
+        shape_info: a shape info instance with type Cylinder
+        station: a PandaStation system
+    """
+
+    assert shape_info.type == Cylinder, "This shape is not a Cylinder"
+
+    """
+    For gripping along the cylinder body:
+        - no collisions
+        - there are acomplished with one bounding box constraint
+            - align the x axis of the gripper with the cylinder axis 
+            - ensure that the gripper is around the axis
+        - penalize distance between the hand and the cylinder center
+        - prefer upright grip
+    for gripping on cylinder top:
+        - todo
+    for gripping the cylinder lengthwise:
+        - constrain the grippers to be within the cross section
+        - penalize deviation from the center
+        - no collisions
+
+    """
+
+    weights = np.array([0,1])
+    norm = np.linalg.norm(weights)
+    assert norm != 0, "invalid weights"
+    weights = weights/norm
+
+    plant = station.get_multibody_plant()
+    assert plant.num_positions() == 7, "This plant is not suitable for inverse kinematics"
+    plant_context = station.GetSubsystemContext(plant, station_context)
+    hand = station.GetHand()
+    hand_frame = plant.GetFrameByName("panda_hand", hand)
+
+    cylinder = shape_info.shape
+    G = shape_info.frame
+    X_WG = G.CalcPoseInWorld(plant_context)
+
+
+
+    costs = []
+    qs = []
+
+    p_tol = 10e-3
+
+    if cylinder.radius() < 0.04:
+        ik = InverseKinematics(plant, plant_context)
+        ik.AddMinimumDistanceConstraint(0, 0.1)
+        ik.AddPositionConstraint(hand_frame,[0.009, 0, 0.1], 
+                G, [p_tol, p_tol, -cylinder.length()/2], 
+                [p_tol,p_tol,cylinder.length()/2])
+        ik.AddPositionConstraint(hand_frame,[-0.009, 0, 0.1], 
+                G, [p_tol, p_tol, -cylinder.length()/2], 
+                [p_tol,p_tol,cylinder.length()/2])
+        prog = ik.prog()
+        q = ik.q()
+        q_nominal = np.array([ 0., 0.55, 0., -1.45, 0., 1.58, 0.])
+        prog.AddQuadraticErrorCost(weights[0]*np.identity(len(q)), 
+                q_nominal, q)
+        AddDeviationFromVerticalCost(prog, q, 
+                plant, plant_context, weight = weights[1])
+        prog.SetInitialGuess(q, q_nominal)
+        result = Solve(prog)
+        cost = result.get_optimal_cost()
+
+        if not result.is_success():
+            cost = np.inf
+
+        costs.append(cost)
+        qs.append(result.GetSolution(q))
+        print(cost)
+
+    if cylinder.length() < 0.08:
+        signs = [-1, 1]
+        for flip in signs:
+            margin = 0.08 - cylinder.length()
+            radius = cylinder.radius()
+            ik = InverseKinematics(plant, plant_context)
+            ik.AddMinimumDistanceConstraint(0, 0.1)
+            ik.AddPositionConstraint(hand_frame,[0, 0.04*flip, 0.1], 
+                    G, [-radius, -radius, cylinder.length()/2], 
+                    [radius, radius, cylinder.length()/2 + margin/2])
+            ik.AddPositionConstraint(hand_frame,[0, -0.04*flip, 0.1], 
+                    G, [-radius, -radius, -cylinder.length()/2 - margin/2], 
+                    [radius, radius, -cylinder.length()/2])
+            ik.AddAngleBetweenVectorsConstraint(hand_frame, 
+                    [0, flip, 0],
+                    plant.world_frame(),
+                    X_WG.rotation().col(2),
+                    0.0,
+                    0.01)
+            prog = ik.prog()
+            q = ik.q()
+            q_nominal = np.array([ 0., 0.55, 0., -1.45, 0., 1.58, 0.])
+            prog.AddQuadraticErrorCost(weights[0]*np.identity(len(q)), 
+                    q_nominal, q)
+            AddDeviationFromVerticalCost(prog, q, 
+                    plant, plant_context, weight = weights[1])
+            prog.SetInitialGuess(q, q_nominal)
+            result = Solve(prog)
+            cost = result.get_optimal_cost()
+
+            if not result.is_success():
+                cost = np.inf
+
+            costs.append(cost)
+            qs.append(result.GetSolution(q))
+            print(cost)
+
+    indices = np.argsort(costs)
+    return qs[indices[0]], min(costs)# return lowest cost
+
 
 def box_grasp_pose(shape_info, station, station_context):
     """
@@ -201,7 +324,8 @@ def box_grasp_pose(shape_info, station, station_context):
     assert shape_info.type == Box, "This shape is not a Box"
 
     """
-    Here we try and grab each of the 3 pairs of faces.
+    Here we try and grab each of the 3 pairs of faces. 
+    Then try the same pairs with the gripper flipped
     The constraints and costs are:
         - no collisions with other objects
         - quadratic error cost from nominal joint position
@@ -237,75 +361,77 @@ def box_grasp_pose(shape_info, station, station_context):
     X_WG = G.CalcPoseInWorld(plant_context)
 
     axes = [0, 1, 2] # x y z
+    signs = [-1,1]
     costs = []
     qs = []
-    for a in axes:
-        #for axis in axes:
-        ik = InverseKinematics(plant, plant_context)
-        ik.AddMinimumDistanceConstraint(0, 0.1)
-        dim = None
-        if a == 0: #x
-            dim = box.width()
-        if a == 1: #y
-            dim = box.depth()
-        if a == 2: #z
-            dim = box.height()
-        margin = 0.08 - dim
-        if (margin < 0):
-            continue 
-        unit_vec = np.zeros(3)
-        unit_vec[a] += 1
-        p_GQu_G = [box.width()/2 + margin/2, box.depth()/2, box.height()/2]
-        p_GQu_G[a] += margin/2
-        p_GQl_G = [-box.width()/2, - box.depth()/2, - box.height()/2]
-        p_GQl_G[a]*= -1.0
-        ik.AddPositionConstraint(hand_frame, 
-                [0, 0.04, 0.1],
-                G,
-                p_GQl_G,
-                p_GQu_G)
+    for flip in signs:
+        for a in axes:
+            #for axis in axes:
+            ik = InverseKinematics(plant, plant_context)
+            ik.AddMinimumDistanceConstraint(0, 0.1)
+            dim = None
+            if a == 0: #x
+                dim = box.width()
+            if a == 1: #y
+                dim = box.depth()
+            if a == 2: #z
+                dim = box.height()
+            margin = 0.08 - dim
+            if (margin < 0):
+                continue 
+            unit_vec = np.zeros(3)
+            unit_vec[a] += 1
+            p_GQu_G = [box.width()/2 + margin/2, box.depth()/2, box.height()/2]
+            p_GQu_G[a] += margin/2
+            p_GQl_G = [-box.width()/2, - box.depth()/2, - box.height()/2]
+            p_GQl_G[a]*= -1.0
+            ik.AddPositionConstraint(hand_frame, 
+                    [0, 0.04*flip, 0.1],
+                    G,
+                    p_GQl_G,
+                    p_GQu_G)
 
-        p_GQu_G = [box.width()/2, box.depth()/2, box.height()/2]
-        p_GQu_G[a]*= -1.0
-        p_GQl_G = [-box.width()/2, - box.depth()/2, - box.height()/2]
-        p_GQl_G[a] -= margin/2
-        ik.AddPositionConstraint(hand_frame, 
-                [0, -0.04, 0.1],
-                G,
-                p_GQl_G,
-                p_GQu_G)
+            p_GQu_G = [box.width()/2, box.depth()/2, box.height()/2]
+            p_GQu_G[a]*= -1.0
+            p_GQl_G = [-box.width()/2, - box.depth()/2, - box.height()/2]
+            p_GQl_G[a] -= margin/2
+            ik.AddPositionConstraint(hand_frame, 
+                    [0, -0.04*flip, 0.1],
+                    G,
+                    p_GQl_G,
+                    p_GQu_G)
 
-        ik.AddAngleBetweenVectorsConstraint(hand_frame, 
-                [0, 1, 0],
-                plant.world_frame(),
-                X_WG.rotation().col(a),
-                0.0,
-                0.01)
+            ik.AddAngleBetweenVectorsConstraint(hand_frame, 
+                    [0, flip*1, 0],
+                    plant.world_frame(),
+                    X_WG.rotation().col(a),
+                    0.0,
+                    0.01)
 
-        prog = ik.prog()
-        q = ik.q()
-        q_nominal = np.array([ 0., 0.55, 0., -1.45, 0., 1.58, 0.])
-        prog.AddQuadraticErrorCost(weights[0]*np.identity(len(q)), 
-                q_nominal, q)
-        AddDeviationFromVerticalCost(prog, q, 
-                plant, plant_context, weight = weights[1])
-        AddDeviationFromBoxCenterCost(prog, q,
-                plant, plant_context, X_WG.translation(),
-                weight = weights[2])
-        prog.SetInitialGuess(q, q_nominal)
-        result = Solve(prog)
-        cost = result.get_optimal_cost()
+            prog = ik.prog()
+            q = ik.q()
+            q_nominal = np.array([ 0., 0.55, 0., -1.45, 0., 1.58, 0.])
+            prog.AddQuadraticErrorCost(weights[0]*np.identity(len(q)), 
+                    q_nominal, q)
+            AddDeviationFromVerticalCost(prog, q, 
+                    plant, plant_context, weight = weights[1])
+            AddDeviationFromBoxCenterCost(prog, q,
+                    plant, plant_context, X_WG.translation(),
+                    weight = weights[2])
+            prog.SetInitialGuess(q, q_nominal)
+            result = Solve(prog)
+            cost = result.get_optimal_cost()
 
-        if not result.is_success():
-            cost = np.inf
+            if not result.is_success():
+                cost = np.inf
 
-        costs.append(cost)
-        qs.append(result.GetSolution(q))
-        print(cost)
+            costs.append(cost)
+            qs.append(result.GetSolution(q))
+            print(cost)
 
 
     indices = np.argsort(costs)
-    return qs[indices[0]] # return lowest cost
+    return qs[indices[0]], min(costs)# return lowest cost
 
 
 def AddDeviationFromVerticalCost(prog, q, plant, plant_context, weight = 1):
